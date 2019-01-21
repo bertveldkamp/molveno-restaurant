f0e8d1a48949c423129d763a4104361a19b7b6ab
bertveldkamp <45998510+bertveldkamp@users.noreply.github.com>
2019-01-21 10:05:41 +0100

    Initial commit

---
+++ src/com/capgemini/molveno/restaurant/Restaurant.java
@@ -0,0 +1,7 @@
+package com.capgemini.molveno.restaurant;
+
+public class Restaurant {
+    public static void main(String[] args){
+        System.out.println("Welcome to the Molveno lake Resort restaurant.");
+    }
+}
