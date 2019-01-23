500f4af1aff4565632dba4939b73448983b18934
djdehaan82 <dennis.de.haan@capgemini.com>
2019-01-23 15:24:50 +0100

    Dennis

---
+++ src/com/capgemini/molveno/restaurant/Reservation.java
@@ -0,0 +1,57 @@
+import.java.time.LocalDate;
+import
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;.java.time.LocaldateTime;
+
+package com.capgemini.molveno.restaurant;
+
+public class Reservation {
+    Private LocalDateTime;
+    Private LocalTime;
+    Private int nrofAdults;
+    private int nrofChildren;
+
+Public Reservation (){
+}
+
+Public LocalDateTime () {
+return
+}
+}
+{
+    Public
+        } class Reservation
+    created =  LocalDateTime.now
+    LocalDateTime.now =< LocalDateTime
+
+
+Public static boolean NrofAdults is Numeric
+Public static boolean NrofChildren is Numeric
+
+constructor
+
+Public static void NrofAdults{}{
+    System.out.println('' please fill in the number of Adults'')
+
+Public static void NrofChildren{}{}
+        System.out.println('' please fill in the number of Children'')
+
+Public Reservarion (int LocalDateTime, int LocalTime, int NrofAdults, int NrofChildren )
+* // Check in database of tijdstip t/m + 2 uur beschikbaar is)
+
+
+
+
+
+If(Reservation =){
+System.out.println("Available")
+}
+
+Else
+        {system.out.println("not available; try another option")
+}
+}
+
+
+Void Available  {Would you like to confirm}
