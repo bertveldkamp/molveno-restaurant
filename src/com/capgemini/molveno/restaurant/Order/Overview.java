e87adef1f6441c2c775ed73d25d3981bc24b7b2a
bertveldkamp <45998510+bertveldkamp@users.noreply.github.com>
2019-01-23 15:11:20 +0100

    Create Overview

---
+++ src/com/capgemini/molveno/restaurant/Order/Overview.java
@@ -0,0 +1,54 @@
+package com.capgemini.molveno.restaurant.Order;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Overview {
+    private HashMap<Consumable, Integer> overviewMap;
+    private double price;
+
+    public Overview(){
+        overviewMap = new HashMap<Consumable, Integer>();
+        this.price = 0;
+    }
+    public void showOverview(){
+
+    }
+    public boolean addToOrder(Consumable item){
+        int ammount = 0;
+        if(overviewMap.containsKey(item)){
+            ammount = overviewMap.get(item);
+        }
+        overviewMap.put(item, ammount +1);
+        return true;
+    }
+    public boolean removeFromOrder(Consumable item){
+        if(overviewMap.containsKey(item)){
+            int ammount = overviewMap.get(item);
+            if(ammount > 1){
+                overviewMap.put(item, ammount - 1);
+                return true;
+            }
+            else if(ammount == 1){
+                overviewMap.remove(item);
+                return true;
+            }
+            else{
+                overviewMap.remove(item);
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
+    }
+    public boolean mergeOrders(HashMap<Consumable, Integer>){
+
+    }
+    public boolean confirmOrder(){
+
+    }
+    public boolean payBill(){
+
+    }
+}
