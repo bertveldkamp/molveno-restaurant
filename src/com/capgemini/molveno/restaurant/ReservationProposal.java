1547485eb82f81aca784eadd25cfd8899e1345c3
Niko van der Vorst <niko.vander.vorst@capgemini.com>
2019-01-23 15:12:04 +0100

    Added ReservationProposal Class - NV

---
+++ src/com/capgemini/molveno/restaurant/ReservationProposal.java
@@ -0,0 +1,42 @@
+package com.capgemini.molveno.restaurant;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class ReservationProposal {
+
+    private LocalDate date;
+    private LocalTime time;
+    private int numberOfAdults;
+    private int numberOfChildren;
+    private boolean validProposal = false;
+
+    public boolean checkAvailable(LocalDate date, LocalTime time, int numberOfAdults, int numberOfChildren){
+        this.date = date;
+        this.time = time;
+        this.numberOfAdults = numberOfAdults;
+        this.numberOfChildren = numberOfChildren;
+
+        checkValidity();
+
+        if(validProposal){
+            //code om te checken of er plek is
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void checkValidity(){
+        if(date.isBefore(LocalDate.now())){
+            System.out.println("Date is in the past");
+            this.validProposal = false;
+        } else {
+            this.validProposal = true;
+        }
+
+
+    }
+
+
+}
