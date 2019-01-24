2c38abf947bbce1088a0f7a9e927e14f3e4165ad
bertveldkamp <45998510+bertveldkamp@users.noreply.github.com>
2019-01-24 13:58:36 +0100

    Improved functionalitiy in Overview

--- src/com/capgemini/molveno/restaurant/Order/Overview.java
+++ src/com/capgemini/molveno/restaurant/Order/Overview.java
@@ -1,54 +1,107 @@
 package com.capgemini.molveno.restaurant.Order;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class Overview {
-    private HashMap<Consumable, Integer> overviewMap;
+    private final HashMap<Consumable, Integer> overviewMap;
     private double price;
 
     public Overview(){
-        overviewMap = new HashMap<Consumable, Integer>();
+        overviewMap = new HashMap<>();
         this.price = 0;
     }
-    public void showOverview(){
 
-    }
-    public boolean addToOrder(Consumable item){
-        int ammount = 0;
-        if(overviewMap.containsKey(item)){
-            ammount = overviewMap.get(item);
+    public void printOverview(){
+        try {
+            overviewMap.forEach((k, v) -> System.out.println("key: " + k + " value:" + v));
         }
-        overviewMap.put(item, ammount +1);
-        return true;
+        catch(NullPointerException e){
+            System.out.println(e.getMessage());
+        }
     }
-    public boolean removeFromOrder(Consumable item){
+
+    public HashMap<Consumable, Integer> getOverviewMap() {
+        return this.overviewMap;
+    }
+    public double getPrice(){
+        return this.price;
+    }
+
+    public void addToOrder(Consumable item){
+        int amount = 0;
         if(overviewMap.containsKey(item)){
-            int ammount = overviewMap.get(item);
-            if(ammount > 1){
-                overviewMap.put(item, ammount - 1);
-                return true;
-            }
-            else if(ammount == 1){
-                overviewMap.remove(item);
-                return true;
-            }
-            else{
-                overviewMap.remove(item);
+            amount = overviewMap.get(item);
+        }
+        overviewMap.put(item, amount +1);
+    }
+    private void addToOrder(Consumable item, int quantity){
+        for(int i = 1; i <= quantity; i++) {
+            addToOrder(item);
+        }
+    }
+
+    public boolean removeFromOrder(Consumable item){
+        try {
+            if (overviewMap.containsKey(item)) {
+                int amount = overviewMap.get(item);
+                if (amount > 1) {
+                    overviewMap.put(item, amount - 1);
+                    return true;
+                } else if (amount == 1) {
+                    overviewMap.remove(item);
+                    return true;
+                } else {
+                    overviewMap.remove(item);
+                    return false;
+                }
+            } else {
                 return false;
             }
         }
-        else{
+        catch (NullPointerException e){
             return false;
         }
     }
-    public boolean mergeOrders(HashMap<Consumable, Integer>){
+
+    private boolean removeFromOrder(Consumable item, Integer quantity) {
+        boolean returnValue = false;
+        for(int i = 1; i <= quantity; i++) {
+            returnValue = removeFromOrder(item);
+            if(!returnValue) {
+                return returnValue;
+            }
+        }
+        return returnValue;
+    }
+
+    private boolean mergeOrders(Overview order){
+
+        try {
+            order.getOverviewMap().forEach(this::addToOrder);
+        }
+        catch (NullPointerException e){
+            return false;
+        }
+        this.price += order.getPrice();
+        return true;
 
     }
-    public boolean confirmOrder(){
 
+    public boolean confirmOrder(Overview orderOverview){
+        //TODO Send to Kitchen Front End
+        //TODO Send to Bar Front End
+        boolean success = orderOverview.mergeOrders(this);
+        try {
+            this.overviewMap.forEach(this::removeFromOrder);
+        }
+        catch (NullPointerException e){
+            return false;
+        }
+        return success;
     }
+
     public boolean payBill(){
-
+        //TODO Generate PDF and EMAIL.
+        return true;
     }
 }
