69627a9b39eafa5952ef880e12fedd7e03db0073
Niko van der Vorst <niko.vander.vorst@capgemini.com>
2019-01-25 14:43:20 +0100

    TablePlanner and ReservatoinProposal update

--- src/com/capgemini/molveno/restaurant/TablePlanner.java
+++ src/com/capgemini/molveno/restaurant/TablePlanner.java
@@ -29,7 +29,7 @@ public class TablePlanner {
             return true;
             } else {
             return false;
-        }
+        } 
     }
 
     public boolean isBetween(int x, int lower, int upper){
