0dbd4e7aaa8499084aa5cdb0d290767ffb813ce9
bertveldkamp <45998510+bertveldkamp@users.noreply.github.com>
2019-02-01 13:55:49 +0100

    Added functionality overview webpage

--- src/main/java/com/capgemini/molveno/restaurant/Controller/OverviewController.java
+++ src/main/java/com/capgemini/molveno/restaurant/Controller/OverviewController.java
@@ -4,19 +4,37 @@ import com.capgemini.molveno.restaurant.Order.*;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/api/overview")
 public class OverviewController {
 
     Menu menu;
+    Overview totalOverview;
     Overview order;
 
     @PostConstruct
     private void init()
     {
+        menu = new Menu("menu");
         order = new Overview();
+
+        menu.add(new Drink("Cola",200,4,"Soft Drinks"));
+        Dish tomatoSoup = new Dish("Tomato Soup","Tomato soup from Italy",20,"Starter");
+        tomatoSoup.addIngredient(new MetaIngredient(4, MetaIngredient.UnitOfMeasurement.KILOGRAM,new Ingredient("Tomato","ACME Italy")));
+        tomatoSoup.addIngredient(new MetaIngredient(10, MetaIngredient.UnitOfMeasurement.GRAM,new Ingredient("Bassilicum","ACME Italy")));
+        menu.add(tomatoSoup);
+        Drink fanta = new Drink("Fanta",200,4,"Soft Drinks");
+        menu.add(fanta);
+        Drink sprite = new Drink("Sprite",200,4,"Soft Drinks");
+        menu.add(sprite);
+
+        order.addToOrder(tomatoSoup,1);
+
     }
 
     @PostMapping("/addToOrder")
@@ -32,11 +50,54 @@ public class OverviewController {
     }
 
     @GetMapping("/overview")
-    public HashMap<Consumable, Integer> getOverview()
+    public List<OrderLine> getOverview()
     {
-        return order.getOverviewMap();
+        HashMap<Consumable, Integer> complexOrderMap = totalOverview.getOverviewMap();
+        List<OrderLine> simpleOrderList = new ArrayList<>();
+        complexOrderMap.forEach((key, value) -> convertMapToList(key, value, simpleOrderList));
+        return simpleOrderList;
     }
 
+    @GetMapping("/order")
+    public List<OrderLine> getorder()
+    {
+        HashMap<Consumable, Integer> complexOrderMap = order.getOverviewMap();
+        List<OrderLine> simpleOrderList = new ArrayList<>();
+        complexOrderMap.forEach((key, value) -> convertMapToList(key, value, simpleOrderList));
+        return simpleOrderList;
+    }
+
+    private class OrderLine{
+        private String name;
+        private double price;
+        private int amount;
+        public OrderLine(String name, double price, int amount){
+            this.name = name;
+            this.price = price;
+            this.amount = amount;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public int getAmount() {
+            return amount;
+        }
+    }
+
+    private void convertMapToList(Consumable item, int quantity, List<OrderLine> simpleOrderList){
+        simpleOrderList.add(new OrderLine(item.getName(), item.getPrice(), quantity));
+    }
+
+    @PostMapping("/order")
+    public void payOrconfider(){
+        totalOverview.mergeOrders(order);
+    }
 
 
 
