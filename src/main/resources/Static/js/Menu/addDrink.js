7d69bfe7d7d7b02461a8a36e2b5034c513fd1008
Morettim1 <maurice.moretti@capgemini.com>
2019-02-07 12:37:33 +0100

    expanded scripts

--- src/main/resources/Static/js/Menu/addDrink.js
+++ src/main/resources/Static/js/Menu/addDrink.js
@@ -7,30 +7,31 @@ addConsumable(consumableJSON);
 function formToJSON()
 {
     var jsonObject = {};
-    if($("#name").val() != "")
-         {
-            jsonObject["name"] = $("#name").val();
-         }
-    if($("#volumeInMilliLiters").val() != "")
-         {
-                 jsonObject["volumeInMilliLiters"] = $("#volumeInMilliLiters").val();
-         }
-    if($("#description").val() != "")
-         {
-            jsonObject["description"] = $("#description").val();
-        }
-    if($("#price").val() > 0)
-        {
-            jsonObject["price"] = $("#price").val();
-        }
-     if($("#course").val() != "")
-        {
-            jsonObject["course"] = $("#course").val();
-        }
-      if($("#bottleCheckBox").val() != "")
-        {
-                    jsonObject["bottleCheckBox"] = $("#bottleCheckBox").val();
-        }
+        if($("#name").val() != "")
+             {
+                jsonObject["name"] = $("#name").val();
+             }
+        if($("#volumeInMilliLiters").val() != "")
+             {
+                     jsonObject["volumeInMilliLiters"] = $("#volumeInMilliLiters").val();
+             }
+        if($("#description").val() != "")
+             {
+                jsonObject["description"] = $("#description").val();
+            }
+        if($("#price").val() > 0)
+            {
+                jsonObject["price"] = $("#price").val();
+            }
+         if($("#course").val() != "")
+            {
+                jsonObject["course"] = $("#course").val();
+            }
+          if($("#bottleCheckBox").val() != "")
+            {
+                        jsonObject["bottleCheckBox"] = $("#bottleCheckBox").val();
+            }
+        return jsonObject;
 }
 
 
