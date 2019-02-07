95b52d272844150788c2313a3cc5bfc9b65d3efb
Morettim1 <maurice.moretti@capgemini.com>
2019-02-07 10:53:31 +0100

    Added addDrink.js Added navbar into addDrink.html. Added Drink tab to navbar.html

---
+++ src/main/resources/Static/js/Menu/addDrink.js
@@ -0,0 +1,45 @@
+function addDrink()
+{
+var consumableJSON = formToJSON();
+addConsumable(consumableJSON);
+}
+
+function formToJSON()
+{
+    var jsonObject = {};
+    if($("#name").val() != "")
+         {
+            jsonObject["name"] = $("#name").val();
+         }
+    if($("#volumeInMilliLiters").val() != "")
+         {
+                 jsonObject["volumeInMilliLiters"] = $("#volumeInMilliLiters").val();
+         }
+    if($("#description").val() != "")
+         {
+            jsonObject["description"] = $("#description").val();
+        }
+    if($("#price").val() > 0)
+        {
+            jsonObject["price"] = $("#price").val();
+        }
+     if($("#course").val() != "")
+        {
+            jsonObject["course"] = $("#course").val();
+        }
+      if($("#bottleCheckBox").val() != "")
+        {
+                    jsonObject["bottleCheckBox"] = $("#bottleCheckBox").val();
+        }
+}
+
+
+$.ajax
+    ({
+        type: "POST",
+        url: "/api/menu/addDrink",
+        contentType: 'application/json',
+        data: JSON.stringify(consumableJSON),
+        dataType:'json'
+    })
+loadMenu();
